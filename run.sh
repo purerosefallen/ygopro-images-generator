e21470065d92934ba6ca281abb6167ec7e3480c0
purerosefallen <78877@qq.com>
2019-05-04 11:18:53 +0800

    use amm cdb

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 export PROCESS_COUNT=$(grep -c processor /proc/cpuinfo)
 
-cd ygopro-database
-git fetch origin master
-git reset --hard FETCH_HEAD
+#cd ygopro-database
+#git fetch origin master
+#git reset --hard FETCH_HEAD
 
 cd ../pics
 git fetch origin master
@@ -16,7 +16,7 @@ cd ..
 #git reset --hard FETCH_HEAD
 #cd ..
 
-#wget -O ./ygopro-database/locales/en-US/cards.cdb https://github.com/shadowfox87/ygopro2/raw/master/cdb/cards.cdb
+wget -O ./ygopro-database/locales/en-US/cards.cdb https://github.com/shadowfox87/ygopro2/raw/master/cdb/cards.cdb
 
 #rm -rf pics/*
 #cp -rf ./YGOTCGOCGHQPics/*.png ./pics/
@@ -30,4 +30,3 @@ ruby --external-encoding=utf-8 generate.rb en-US
 
 pm2 list
 ls ./mse-sets/ | xargs -I {} -P $PROCESS_COUNT ./mse.sh {}
-
