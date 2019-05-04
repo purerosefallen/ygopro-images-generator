399fcba22aaba50457a2f5d40f93fb1bc1700524
purerosefallen <78877@qq.com>
2019-05-04 13:41:27 +0800

    enable multi thread in imagemagick

--- mse.sh
+++ mse.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export PROCESS_COUNT=$(grep -c processor /proc/cpuinfo)
 export MSE_PATH=./mse-sets/$1
 
 echo "Generating $MSE_PATH."
@@ -32,7 +33,7 @@ pm2 delete $PROCESS_ID
 
 echo "Resizing images of $MSE_PATH."
 
-ls $TMP_PATH/images/ | sed -e "s/\.png$//" | xargs -I {} convert $TMP_PATH/images/{}.png -resize 322x433! $PWD/images/picture/card/{}.jpg
+ls $TMP_PATH/images/ | sed -e "s/\.png$//" | xargs -I {} -P PROCESS_COUNT convert $TMP_PATH/images/{}.png -resize 322x433! $PWD/images/picture/card/{}.jpg
 rm -rf $TMP_PATH
 
 echo "Finished generating $MSE_PATH."
